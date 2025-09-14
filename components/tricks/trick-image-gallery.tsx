d2ee7a84b27bc34024e5a3601de2c8e8f99291f8
jado66 <jado66@gmail.com>
2025-09-13 20:28:43 -0600

    Fixed client side error

--- components/tricks/trick-image-gallery.tsx
+++ components/tricks/trick-image-gallery.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { TrickWithLinkedPrerequisites } from "@/types/trick";
 
 interface TrickImageGalleryProps {
--- components/tricks/video-tutorials.tsx
+++ components/tricks/video-tutorials.tsx
@@ -1,3 +1,4 @@
+("");
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Play, ExternalLink } from "lucide-react";
 import { TrickWithLinkedPrerequisites } from "@/types/trick";
