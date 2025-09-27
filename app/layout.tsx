7debabe40e4eb38976269eb0b21f2067c7116212
jado66 <jado66@gmail.com>
2025-09-27 13:41:46 -0600

    pwa fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,12 +50,25 @@ export default async function RootLayout({
       <head>
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
         <link rel="icon" href="/favicon/favicon.ico" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Trickipedia" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="application-name" content="Trickipedia" />
         <meta name="theme-color" content="#000000" />
